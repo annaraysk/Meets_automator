f076202295243487419b1e5bf98270870fdeef67
array <annaraykalashetty1999@gmail.com>
2020-09-19 20:05:30 +0530

    Added Time Table
    Time table is ready, Wait for implementation of auto feature.
    Changed class end Feature. Now class will end 10 minutes prior to next class, change some 600 to 300 in script.py to end class 5 min before next class.

--- links_file.py
+++ links_file.py
@@ -1,6 +1,6 @@
 
 #list of class, index is important 
-subject_list = ["PADP","DSML","CG","BDA_Sir","BDA_Maam","CNS"]
+subject_list = ["PADP","DSML","CG","BDA_Sir","BDA_Maam","CNS","AERO"]
 
 # class links for now.
 dictionary_of_links = {}
@@ -10,3 +10,13 @@ dictionary_of_links["CG"]= "https://meet.google.com/qas-jdxy-gsr"
 dictionary_of_links["BDA_Sir"]="http://meet.google.com/xtu-yidv-fng"
 dictionary_of_links["BDA_Maam"] = "https://meet.google.com/epx-pktt-cib"
 dictionary_of_links["CNS"] = "https://meet.google.com/yrk-pitb-vnn"
+
+# time table 
+
+tt = [
+    [0,1,5,6],
+    [2,4,5,1],
+    [4,5,0,6],
+    [5,2,4,6],
+    [1,4,2,0]
+]
--- script.py
+++ script.py
@@ -5,20 +5,51 @@ from selenium import webdriver
 from selenium.webdriver.chrome import service
 from links_file import dictionary_of_links
 from links_file import subject_list
+from links_file import tt
 from time import sleep
 from webdriver_manager.chrome import ChromeDriverManager
+import datetime
 
 #my important variables
 class_time = 50*60
 cookie_file1 = "/home/array/Documents/cookie1.data"
 cookie_file2 = "/home/array/Documents/cookie2.data"
+week_days = ["Mon", "Tue", "Wed", "Thu", "Fri"]
 
 parser = argparse.ArgumentParser(description="Meets automator Script\n"+str(subject_list)+"\nMake sure you use right index to start class.", formatter_class=argparse.RawTextHelpFormatter)
 
 parser.add_argument("-s", "--start", help="Start the specified class.",action="store",type=str)
 parser.add_argument("-l","--links", help="Show Links.", action="store_true")
 parser.add_argument("--saveCookie", help="saves cookie to local file, make sure you have a cookie file to prevent login issues for class.",action="store_true")
+parser.add_argument("-a","--auto",help="Automates all 4 classes, based on day and timetable.",action="store_true")
+parser.add_argument("-t","--timetable", help="Shows timetable", action="store_true")
 
+def attend_class(class_link):
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.get("https://accounts.google.com/login")
+    cookies = pickle.load(open(cookie_file1,"rb"))
+    for i in cookies:
+        if 'expiry' in i:
+            i['expiry'] = int(i['expiry'])
+        driver.add_cookie(i)
+    driver.get(class_link)
+    cookies = pickle.load(open(cookie_file2,"rb"))
+    for i in cookies:
+        driver.add_cookie(i)
+    sleep(2)
+    driver.find_element_by_xpath("""//*[@id="yDmH0d"]/div[3]/div/div[2]/div[3]/div/span/span""").click()
+    sleep(2)
+    driver.find_element_by_xpath("""//*[@id="yDmH0d"]/c-wiz/div/div/div[5]/div[3]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div[1]/span/span""").click()
+    delta = datetime.timedelta(hours=1)
+    now = datetime.datetime.now()
+    next_hour = (now + delta).replace(microsecond=0, second=0, minute=2)
+    class_time = (next_hour - now).seconds - 600
+    if class_time<0:
+        class_time=1  
+    sleep(class_time)
+    driver.find_element_by_xpath("""//*[@id="ow3"]/div[1]/div/div[5]/div[3]/div[9]/div[2]/div[2]/div""").click()
+    sleep(2)
+    driver.close()
 
 args = parser.parse_args()
 
@@ -46,26 +77,19 @@ if args.saveCookie:
     driver.close()
 
 if args.start:
-    driver = webdriver.Chrome(ChromeDriverManager().install())
-    driver.get("https://accounts.google.com/login")
-    cookies = pickle.load(open(cookie_file1,"rb"))
-    for i in cookies:
-        if 'expiry' in i:
-            i['expiry'] = int(i['expiry'])
-        driver.add_cookie(i)
     try:
-        driver.get(dictionary_of_links[subject_list[int(args.start)]])
+        class_link=dictionary_of_links[subject_list[int(args.start)]]
+        attend_class(class_link)
     except:
-        print("Not valid index. Choose among this list")
-        print(subject_list)
-    cookies = pickle.load(open(cookie_file2,"rb"))
-    for i in cookies:
-        driver.add_cookie(i)
-    sleep(2)
-    driver.find_element_by_xpath("""//*[@id="yDmH0d"]/div[3]/div/div[2]/div[3]/div/span/span""").click()
-    sleep(2)
-    driver.find_element_by_xpath("""//*[@id="yDmH0d"]/c-wiz/div/div/div[5]/div[3]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div[1]/span/span""").click()
-    sleep(class_time)
-    driver.find_element_by_xpath("""//*[@id="ow3"]/div[1]/div/div[5]/div[3]/div[9]/div[2]/div[2]/div""").click()
-    sleep(2)
-    driver.close()
+        print("Choose right index.")
+
+if args.timetable:
+    ttlist = []
+    for i in range(5):
+        temp=[week_days[i]]
+        for j in range(4):
+            temp.append(subject_list[tt[i][j]])
+        ttlist.append(temp)
+    print(tabulate(ttlist,[" ","9 - 10","10 - 11","11 - 12","12 - 1"],"fancy_grid"))
+    
+        
